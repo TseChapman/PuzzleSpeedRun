d469b1291bcbdcb1de297e774b18e70d6d3266a3
Eunmin Lee <u.leahlee@gmail.com>
2021-05-17 01:30:27 -0700

    Playtest map updated

---
+++ ASL/Assets/TutorialVideoArea.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialVideoArea : MonoBehaviour {
+    public GameObject[] toPlay;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "PCPlayer" || other.gameObject.name == "VR Rig")
+        {
+            activateVideo();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "PCPlayer" || other.gameObject.name == "VR Rig")
+        {
+            deactivateVideo();
+        }
+    }
+    private void activateVideo()
+    {
+        foreach (GameObject item in toPlay)
+        {
+            item.SetActive(true);
+        }
+    }
+
+    private void deactivateVideo()
+    {
+        foreach (GameObject item in toPlay)
+        {
+            item.SetActive(false);
+        }
+    }
+
+}
